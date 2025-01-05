b0abe04e232bc0a5f372aa9461c53e91df4a8c9c
Tegan Jain <teganjain527@gmail.com>
2025-01-05 14:27:48 +0530

    small changes to font size/color etc also from figma devmode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,10 +22,10 @@ const gold = Goldman({
 const HomePage: NextPage = () => {
   return (
     <PageLayout title="Homepage" description="Welcome to next-web-template">
-      <Typography variant="h1" className={`${syne.className} text-center px-4 sm:px-8 lg:px-16 py-4`}  >
+      <Typography variant="h1" className={`text-center text-[#d5ddf3] text-[70px] font-normal ${syne.className}`}>
         Decentralized Autonomous<br />Agentic Organization
       </Typography>
-      <Typography variant="h3" className={`${syne.className} text-center px-4 sm:px-8 lg:px-60 py-4`}>
+      <Typography variant="h3" className={`w-[790px] text-center text-[#d5ddf3] text-2xl font-normal ${syne.className}`}>
         Where autonomous agents meet decentralized innovation, driving seamless collaboration for a smarter future.
       </Typography>
 
@@ -36,7 +36,7 @@ const HomePage: NextPage = () => {
       >
         <Button variant="connect"
           className={`w-[300px] h-[60px] px-2 py-3 bg-white rounded-[100px] shadow-[4px_4px_20px_0px_rgba(255,255,255,0.28)] shadow-[-4px_-4px_40px_0px_rgba(255,255,255,0.28)] border border-[#bedaff] justify-center items-center gap-2 inline-flex"`}>
-          <div className="text-center text-black text-2xl font-normal goldMan leading-tight tracking-wide">
+          <div className="text-center text-black text-2xl font-normal goldman leading-tight tracking-wide">
             Whitepaper
           </div>
         </Button>
