b48f7824a33467a82bfa073ba86ad73735f09790
Tegan Jain <teganjain527@gmail.com>
2025-01-06 22:33:03 +0530

    add middleware for subdomain, and update links

---
+++ src/pages/app/index.tsx
@@ -0,0 +1,12 @@
+import { PageLayout } from '@/components/page-layout';
+import React from 'react';
+
+const AppHome: React.FC = () => {
+  return (
+    <PageLayout>
+      <p> app.daao.ai </p>
+    </PageLayout>
+  );
+};
+
+export default AppHome;
