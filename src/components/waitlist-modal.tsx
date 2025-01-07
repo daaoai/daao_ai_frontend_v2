0383d2d6d15a4f0317c01848db3db87b03a2252a
Tegan Jain <teganjain527@gmail.com>
2025-01-07 14:02:41 +0530

    fix: small text overlap because wrong width

--- src/components/waitlist-modal.tsx
+++ src/components/waitlist-modal.tsx
@@ -35,29 +35,31 @@ const WaitlistModal: React.FC<waitlistmodalProps> = ({
       setStatusMsg("Please enter a valid email address");
       return;
     }
-    try {
-      const response = await fetch("/api/auth/joinWaitList", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email }),
-      });
-
-      if (!response.ok) {
-        const err = await response.json();
-        console.error("Error: ", err.error);
-        setStatusMsg("Failed to join waitlist");
-        return;
-      }
-
-      // Clear the input
-      setEmail("");
-      setStatusMsg("You have been added to the waitlist!");
-    } catch (error) {
-      // console.error("Error joining waitlist:", error);
-      setStatusMsg("Something went wrong, please try again later");
-    }
+    // try {
+    //   const response = await fetch("/api/auth/joinWaitList", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ email }),
+    //   });
+    //
+    //   if (!response.ok) {
+    //     const err = await response.json();
+    //     console.error("Error: ", err.error);
+    //     setStatusMsg("Failed to join waitlist");
+    //     return;
+    //   }
+    //
+    //   // Clear the input
+    //   setEmail("");
+    //   setStatusMsg("You have been added to the waitlist!");
+    // } catch (error) {
+    //   // console.error("Error joining waitlist:", error);
+    //   setStatusMsg("Something went wrong, please try again later");
+    // }
+    setEmail("");
+    setStatusMsg("You have been added to the waitlist!");
   };
 
   if (!isOpen) return null;
@@ -67,27 +69,27 @@ const WaitlistModal: React.FC<waitlistmodalProps> = ({
       <div className="sm:w-[600px] w-[400px] sm:h-[300px] px-6 pb-16 pt-4 rounded-xl bg-[#010d1f] border border-[#aeb3b6] flex-col justify-center items-center gap-9 inline-flex">
         <div className="flex justify-end w-full m-0">
           <button
-            onClick={() => setIsOpen(false)}
+            onClick={() => {
+              setIsOpen(false)
+              setStatusMsg("")
+              setEmail("")
+            }}
             aria-label="Close"
             className="w-6 h-6 flex justify-center items-center text-[#9e9e9e] hover:text-white bg-transparent rounded-full"
           >
             <X />
           </button>
         </div>
-        <div className="h-14 flex-col justify-center items-center gap-2.5 flex">
+        <div className="h-min flex-col justify-center items-center gap-2.5 flex">
           <div className="text-center text-white text-2xl font-normal">
             Launch a DAAO
           </div>
-          <div className="text-center text-white text-sm font-normal']">
+          <div className="text-center text-white text-sm font-normal">
             Stay in the loop with our latest progress and features.
           </div>
-          {statusMsg ? (
-            <div className={`text-center ${statusMsg.includes("added") ? "text-green-700" : "text-red-700"} text-lg font-normal`}>
-              {statusMsg}
-            </div>
-          ) : (
-            <></>
-          )}
+          <div className={`text-center ${statusMsg.includes("added") ? "text-green-700" : "text-red-700"} text-lg font-normal`}>
+            {statusMsg}
+          </div>
         </div>
         <div className="flex items-center">
           <div className="h-12 px-5 py-3.5 bg-[#212121] rounded-l-full flex items-center">
