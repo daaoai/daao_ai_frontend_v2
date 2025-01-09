12a9efc4506c20ca59a83fc6d11b551722f09773
itsme-boii <thaaa367@gmail.com>
2025-01-09 21:34:03 +0530

    email_to_publickey_migration

--- prisma/migrations/20250108193437_init/migration.sql
+++ prisma/migrations/20250108193437_init/migration.sql
@@ -1,11 +1,8 @@
 -- CreateTable
 CREATE TABLE "WaitingList" (
     "id" SERIAL NOT NULL,
-    "publicKey" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "WaitingList_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "WaitingList_publicKey_key" ON "WaitingList"("publicKey");
