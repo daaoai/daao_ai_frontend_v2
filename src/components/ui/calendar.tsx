6d9efd57c28333adb589c9d3415749415cc8e23d
Tegan Jain <teganjain527@gmail.com>
2025-01-24 23:07:50 +0530

    fix: i am literally typecasting to any. this is not good. anyway i hope it builds now

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -58,13 +58,13 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        IconLeft: ({ className, ...props }: any) => (
           <ChevronLeftIcon className={cn("h-4 w-4", className)} {...props} />
         ),
-        IconRight: ({ className, ...props }) => (
+        IconRight: ({ className, ...props }: any) => (
           <ChevronRightIcon className={cn("h-4 w-4", className)} {...props} />
         ),
-      }}
+      } as any}
       {...props}
     />
   )
