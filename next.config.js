ae940c6363beca25b2d7ed475c3cbdcb128957b4
Kanwar Singh <kanwarsingh@Kanwars-MacBook-Air.local>
2025-02-19 00:43:22 +0530

    removed unnecessary comments from next config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   trailingSlash: true,
-//   distDir: 'build',
-//   images: {
-//     unoptimized: true,
-//   },
-//   sassOptions: {
-//     additionalData: `$var: red;`,
-//   },
-//   webpack: (config) => {
-//     config.resolve.fallback = { fs: false, net: false, tls: false };
-//     return config;
-//   },
-// };
-
-// module.exports = nextConfig;
-
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
