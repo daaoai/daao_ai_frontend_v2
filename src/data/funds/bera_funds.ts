7c96d0611eb250a6983ad05f77584371515d4f70
Kanwar Singh <kanwarsingh@Kanwars-MacBook-Air.local>
2025-05-06 15:06:51 +0530

    fixes

--- src/data/funds/bera_funds.ts
+++ src/data/funds/bera_funds.ts
@@ -10,7 +10,7 @@ export const beraFunds: Record<string, FundDetails> = {
       'DeFAI Venture DAO is a DeFAI Investment DAO dedicated to advancing the DeFAI movement by strategically investing in AI Agents and AI-focused DAOs on Bera. As a collective force in decentralized AI finance, $BERA empowers the AI-driven movement on Bera, fostering the growth of autonomous, AI-powered ecosystems.',
     address: '0x402f35e11cC6E89E80EFF4205956716aCd94be04',
     token: zeroAddress,
-    imgSrc: '/assets/batman.jpg',
+    imgSrc: '/assets/berachain.svg',
     status: 'trading',
     isManageLiquidityEnabled: true,
     dexInfo: {
